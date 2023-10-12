781febb5825598f233d36f7314e3977a2508bc7b
Aiman Asif <72918553+aimanasif27@users.noreply.github.com>
2023-10-12 15:06:04 +0000

    Added some other features

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 import os
 import discord
 from discord.ext import commands
-
 from dotenv import load_dotenv
+
+import requests
+from bs4 import BeautifulSoup
 
 # Load environment variables from .env file
 load_dotenv()
-
 # Access the TOKEN environment variable
 TOKEN = os.getenv('TOKEN')
 
@@ -15,33 +16,68 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 # Create a bot instance with intents and command prefix
-bot = commands.Bot(command_prefix='!cx_', intents=intents)
+bot = commands.Bot(command_prefix='!cx ', intents=intents)
 
 # Event: Bot is ready
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user.name}')
 
-# Command: Hello
+# Command: Register
 @bot.command()
-async def hello(ctx):
-    await ctx.send('Hello!')
+async def register(ctx, *args):
+    if not args:
+        await ctx.send("You didn't provide an event.")
+    else:
+        sentence = ' '.join(args)
+        await ctx.send(f'We will be adding this functionality soon to register you for {sentence}!')
     
-# Command: Ping
+# Command: ndaysofcode
 @bot.command()
 async def ndaysofcode(ctx, *args):
     if not args:
         await ctx.send("You didn't provide a link.")
     else:
-        sentence = ' '.join(args)
-        response = f'You said: {sentence}'
-        await ctx.send(response)
+        link = ' '.join(args)
+        if link.startswith('https://www.linkedin.com') or link.startswith('https://twitter.com'):
+            if checkthepost(link):
+                await ctx.send("We noted your response!")
+            else:
+                await ctx.send("You provided incorrect LinkedIn or Twitter post link!")
+        else:
+            await ctx.send("You didn't provid correct LinkedIn or Twitter post link!")
+
+# Command: Help
+@bot.command()
+async def _help(ctx):
+    await ctx.send('Use `!cx ndaysofcode <link>` to post your submission\nUse `!cx register <event_name>` to Register for the event')
 
 # Error handler for CommandNotFound
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
-        await ctx.send("Command not found. Use `!help` to see available commands.")
+        await ctx.send("Command not found. Use `!cx _help` to see available commands.")
 
 
 bot.run(TOKEN)
+
+
+def checkpost(url:String) -> bool:
+
+    # Send an HTTP GET request to the URL
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the HTML content of the page
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        # Find all the article titles by inspecting the HTML structure
+        articles = soup.find_all("h2", class_="article-title")
+
+        # Iterate through the list of articles and print their titles
+        for article in articles:
+            print(article.text)
+    else:
+        print("Failed to retrieve the web page. Status code:", response.status_code)
+
